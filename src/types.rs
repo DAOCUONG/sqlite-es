72eaed81c15dd124f731f662787e7057b3f31b6f
LexRayBuildBot <>
2022-11-17 13:48:12 -0600

    sqlite Error

---
+++ src/types.rs
@@ -0,0 +1,7 @@
+use crate::PostgresEventRepository;
+use cqrs_es::persist::PersistedEventStore;
+use cqrs_es::CqrsFramework;
+
+/// A convenience type for a CqrsFramework backed by
+/// [PostgresStore](struct.PostgresStore.html).
+//pub type SqliteCqrs<A> = CqrsFramework<A, PersistedEventStore<SqliteEventRepository, A>>;
